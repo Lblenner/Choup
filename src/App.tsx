a98561397d3adf0bbed7604959e6986cb2ff1676
Louis <louis.blenner@gmail.com>
2020-09-15 03:24:31 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { CSSProperties, useState } from 'react';
+import FlareComponent from 'flare-react';
+import cactus from './Choup.flr';
+
+const size = 200
 
 function App() {
+
+  const [grabbed, setGrabbed] = useState(false)
+  const [XY, setXY] = useState({ x: window.innerWidth/2 - size/2, y: window.innerHeight/3 - size/2 })
+
+  const mouseMoved = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (grabbed) {
+      setXY({ x: e.clientX - size / 2, y: e.clientY - size / 2 })
+    }
+  }
+
+  const bim: CSSProperties = {
+    position: "absolute",
+    left: XY.x,
+    top: XY.y,
+    width: size,
+    height: size
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={center} onMouseMove={(e) => mouseMoved(e)}>
+      <div style={text}>Click and drop me !</div>
+      <div style={bim} onClick={() => setGrabbed(!grabbed)}>
+        <FlareComponent width={200} height={200} transparent={true} animationName={grabbed ? "Grabbed" : "Flying"} file={cactus} />
+      </div>
     </div>
   );
+
+}
+
+const text: CSSProperties = {
+  fontSize: "50px"
+}
+
+const center: CSSProperties = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "100%",
+  height: "100vh",
+  backgroundColor: "lightgrey",
 }
 
 export default App;
